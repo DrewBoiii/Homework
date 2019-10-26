839e3018d0da5eefbb388282f95ed501d9ebc55b
Drew <goodchickenboy@mail.ru>
2019-10-26 21:34:01 +0400

    fix main controller tests, add test cars and editor views tests

--- src/test/java/example/drew/homework/web/controller/MainControllerTest.java
+++ src/test/java/example/drew/homework/web/controller/MainControllerTest.java
@@ -25,6 +25,7 @@ public class MainControllerTest {
     public void getIndexPage_whenAvailableUrlValueIsSlashHome_statusOk() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/home").accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().encoding("UTF-8"))
                 .andExpect(MockMvcResultMatchers.view().name("index"))
                 .andDo(MockMvcResultHandlers.print());
     }
@@ -33,6 +34,7 @@ public class MainControllerTest {
     public void getIndexPage_whenAvailableUrlValueIsSlashHomeArgLangEn_statusOk() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/home?lang=en").accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().encoding("UTF-8"))
                 .andExpect(MockMvcResultMatchers.view().name("index"))
                 .andDo(MockMvcResultHandlers.print());
     }
@@ -41,6 +43,7 @@ public class MainControllerTest {
     public void getIndexPage_whenAvailableUrlValueIsSlashHomeArgLangRu_statusOk() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/home?lang=ru").accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().encoding("UTF-8"))
                 .andExpect(MockMvcResultMatchers.view().name("index"))
                 .andDo(MockMvcResultHandlers.print());
     }
@@ -49,6 +52,7 @@ public class MainControllerTest {
     public void getIndexPage_whenAvailableUrlValueIsSlash_statusOk() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().encoding("UTF-8"))
                 .andExpect(MockMvcResultMatchers.view().name("index"))
                 .andDo(MockMvcResultHandlers.print());
     }
@@ -66,6 +70,22 @@ public class MainControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/login").accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.view().name("login"))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    public void getCarsPage_whenUnavailableUrl_statusIsFoundAndRedirectToLoginPage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/cars").accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isFound())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("http://localhost/login"))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    public void getEditorPage_whenUnavailableUrl_statusIsFoundAndRedirectToLoginPage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/editor").accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isFound())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("http://localhost/login"))
                 .andDo(MockMvcResultHandlers.print());
     }
 
