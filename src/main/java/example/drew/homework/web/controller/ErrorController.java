594b4e38fa84c5ff80b52cbc5b4ff5554f81a067
DrewBoiii <dhdhdh06@mail.ru>
2019-11-02 14:34:59 +0400

    add error handling, add error page and error controller

---
+++ src/main/java/example/drew/homework/web/controller/ErrorController.java
@@ -0,0 +1,27 @@
+package example.drew.homework.web.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Slf4j
+@ControllerAdvice
+public class ErrorController {
+
+    @ExceptionHandler(Throwable.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String exception(final Throwable throwable, final Model model) {
+        log.error("Exception during execution of SpringSecurity application", throwable);
+//        String errorMessage = (throwable != null ? throwable.getMessage() : "Unknown error");
+        String errorMessage = "unknown error";
+        if(throwable != null){
+            errorMessage = throwable.getMessage();
+        }
+        model.addAttribute("errorMessage", errorMessage);
+        return "error";
+    }
+
+}
