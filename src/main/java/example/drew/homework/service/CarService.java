5703c44f6f12215d9b2dd12eeadebbc6e847bfaf
Drew <goodchickenboy@mail.ru>
2019-11-10 19:08:37 +0400

    add role moderator; fix change logs

--- src/main/java/example/drew/homework/service/CarService.java
+++ src/main/java/example/drew/homework/service/CarService.java
@@ -2,8 +2,10 @@ package example.drew.homework.service;
 
 import example.drew.homework.exception.CarNotFoundException;
 import example.drew.homework.persistence.model.Car;
+import example.drew.homework.util.RoleConstants;
 import example.drew.homework.web.dto.CarDto;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.security.access.annotation.Secured;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,8 +18,13 @@ public interface CarService {
     List<Car> getCarsByUsername(String username);
 
     Optional<Car> getCarById(Long id) throws CarNotFoundException;
+
     void saveCar(CarDto car);
+
+    @Secured(RoleConstants.ROLE_MODERATOR)
     void deleteCarById(Long id);
+
+    @Secured(RoleConstants.ROLE_MODERATOR)
     void updateCar(CarDto car);
 
 }
