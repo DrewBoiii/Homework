32946e00bc2d0ae51fd9a8d9a8ab458740a59324
DrewBoiii <dhdhdh06@mail.ru>
2019-11-19 17:57:32 +0400

    add flexible pagination mechanism

--- src/main/java/example/drew/homework/web/controller/MainController.java
+++ src/main/java/example/drew/homework/web/controller/MainController.java
@@ -10,19 +10,27 @@ import example.drew.homework.web.dto.UserDto;
 import example.drew.homework.web.filter.criteria.CarCriteria;
 import example.drew.homework.web.filter.spec.CarSpecification;
 import org.springframework.context.MessageSource;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 public class MainController {
+
+    private static final int MAX_ELEMENTS_PER_PAGE = 6;
+    private static final int MAX_INDEXES_RANGE_PER_PAGE = 5;
 
     private CarService carService;
     private UserService userService;
@@ -45,10 +53,18 @@ public class MainController {
     }
 
     @GetMapping("/home")
-    public String getIndexPage(Model model, @ModelAttribute("car_criteria") CarCriteria carCriteria) {
-        CarSpecification carSpecification = new CarSpecification(carCriteria);
-
-        model.addAttribute("cars", carService.getCarsBySpecification(carSpecification, PageRequest.of(0, 3, Sort.by("createdAt").descending())));
+    public String getIndexPage(Model model,
+                               @ModelAttribute("car_criteria") CarCriteria carCriteria,
+                               @RequestParam(required = false) Optional<Integer> page) {
+        Specification<Car> carSpecification = new CarSpecification(carCriteria);
+        model.addAttribute("viewRange", MAX_INDEXES_RANGE_PER_PAGE);
+        Page<Car> carPages =  carService.getCarsBySpecification(carSpecification, PageRequest.of(page.orElse(0), MAX_ELEMENTS_PER_PAGE, Sort.by("createdAt").descending()));
+        int totalPages = carPages.getTotalPages();
+        List<Integer> pageIndexes = IntStream.rangeClosed(1, totalPages).boxed().collect(Collectors.toList());
+        model.addAttribute("page_indexes", pageIndexes);
+        model.addAttribute("active_page", page.orElse(0));
+        model.addAttribute("view_range", MAX_INDEXES_RANGE_PER_PAGE);
+        model.addAttribute("cars", carPages);
 
         return "index";
     }
